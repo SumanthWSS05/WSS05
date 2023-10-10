116489bf5cbb3af1dd8e2085cf2e9e86d209e3c4
Sumanth <Sumanth@TYSS-Sumanth>
2023-10-10 23:31:20 +0530

    Added new Script

---
+++ WSS05.Project.SeleniumExpert/src/test/java/login/TC_BMW.java
@@ -0,0 +1,19 @@
+package login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_BMW {
+
+	@Test(groups = "watch")
+	public void explore() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.bmw.in/en/index.html");
+		Thread.sleep(1000);
+		driver.quit();
+	}
+}
